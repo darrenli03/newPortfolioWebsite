b2e586fdc65cbda76a56f1a24bc3864f39785ee1
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-12-31 19:02:19 -0500

    improvements in about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import {
-  faAngular,
-  faCss3,
-  faGitAlt,
+  faJava,
+  faPython,
+  faCss3Alt,
   faHtml5,
   faJsSquare,
   faReact,
@@ -12,8 +12,11 @@ import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
+
+
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+
 
   useEffect(() => {
     return setTimeout(() => {
@@ -33,30 +36,30 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm currently a Computer Science student at Duke University eager to 
+            I'm currently a Computer Science student at Duke University eager to
             familiarize myself with the tech industry through internships and research.
           </p>
           <p align="LEFT">
-            My main skills include Java, Python, and web development (Javascript, HTML, CSS).
+            My main skills include Java, Python, and web development (Javascript, React, HTML, and CSS).
             I'm a quick and dedicated learner, and I look forward to developing my skills through
             my future experiences!
           </p>
           <p>
             On the side, I like to play violin, chess, and video games, read (my favorite genres are
-             sci-fi and fantasy), and watch movies. I also love to try new foods and explore new places!
+            sci-fi and fantasy), and watch movies. I also love to try new foods and explore new places!
           </p>
         </div>
 
         <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
             </div>
             <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" transform={{ rotate: -90 }}/>
             </div>
             <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+              <FontAwesomeIcon icon={faPython} color="#008000" />
             </div>
             <div className="face4">
               <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
@@ -65,7 +68,7 @@ const About = () => {
               <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
             </div>
             <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+              <FontAwesomeIcon icon={faCss3Alt} color="#264de4" />
             </div>
           </div>
         </div>
