dc7842fecaf924d47f80173f94977143588eef3a
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2024-01-04 19:42:08 -0500

    changed icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import newLogo from '../../assets/images/newicon.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faLinkedin,
@@ -30,7 +31,8 @@ const Sidebar = () => {
         onClick={() => setShowNav(false)}>
         {/* <img src={LogoS} alt="Logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
-        <h1> DL </h1>
+        {/* <h1> DL </h1> */}
+        <img src={newLogo} alt="Logo" />
       </Link>
       <nav className={showNav ? 'mobile-show' : ''}>
         <NavLink 
